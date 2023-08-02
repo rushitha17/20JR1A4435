aa552a0701e2b720f807653bbb380309a39f2a50
rushitha17 <85926492+rushitha17@users.noreply.github.com>
2023-08-02 12:34:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home.js";
+import SearchTrain from "./pages/Search.js";
+ 
+const App=()=>{
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/search" element={<SearchTrain/>}/>
+        </Routes>
+      </BrowserRouter>
+    </> 
+  );
+}
+
+export default App;
